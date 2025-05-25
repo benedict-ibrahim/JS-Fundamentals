1f96d8ec5f4e98d07d853f8864aa8e3a712f528e
Benedict Ibrahim <benedictnomsuibrahim@gmail.com>
2025-05-25 13:20:49 +0100

    process.argv: Wrote a script that print the first argmunent passed to it

---
+++ 3-value_argument.js
@@ -0,0 +1,17 @@
+/*
+Write a script that prints the first argument passed to it:
+
+If no arguments are passed to the script, print “No argument”
+You must use console.log(...) to print all output
+You are not allowed to use var
+You are not allowed to use length
+*/
+
+const firstArgs = process.argv[2];
+
+// Checks if no argument is passed
+if (firstArgs === undefined) {
+    console.log('No argument');
+} else {
+    console.log('School');
+}
